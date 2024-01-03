a63048afa8278e8a4ba7e3a0abd88e6c3f32cc0f
Eduarda Lieutaud <lieutaudev@gmail.com>
2024-01-03 16:05:05 -0300

    projeto finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Projetos (Todos diponíveis no GitHub)",
+    "description": "JAVA- System Chess, Conta bancária, Calcular horas de trabalho, Site de aluguel de carros || Pokedex-  HTML e CSS ",
+    "photoCover":""
+  },
+  {
+    "id":"2",
+    "title": "Meus certificados",
+    "description": "DIO- Desenvolvimento JAVA com Cloud AWS || Descubra a nuvem AWS || GFT Start woman JAVA ",
+    "photoCover":""
+  },
+  
+]
